bb3a1e0223ff641f5df223a13e5b87b5b026ff0e
Chris Huang <chris2004huang@yahoo.com>
2018-02-06 10:45:44 -0700

    Modified in VS

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //add code here
-            //Modified again
+            //add code here 
+            //Modified again in Github
+            //Modified in VS
         }
     }
 }
